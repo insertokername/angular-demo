10f00e6254476a6783a2a2aeb2686a6988e864d8
insertokername <111150085+insertokername@users.noreply.github.com>
2024-04-22 18:40:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# AngularDemo
+
+**dependecies**
+- nodejs
+- npm
+
+*if you are using **nix** and have **flakes** enabled you can use `nix develop`*
+
+**installing pkgs**
+`npm install`
+
+**running**
+`npm run start`
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        buildInputs = [
+	  pkgs.nodejs
+	];
+        shellHook = ''
+          echo entered dev shell!  
+        '';
+      };
+  };
+}
